a7e9a8e856d99101769c5553666ae04a7bc208b8
Zaid Khider <ZKhider@spartaglobal.com>
2019-08-06 20:27:57 +0100

    made a read me file

--- raindrop_function.py
+++ raindrop_function.py
@@ -1,17 +1,17 @@
 def raindrop_function(n):
     output = ""
 
-    if number_inputted % 3 == 0:
+    if n % 3 == 0:
         output += "Pling"
 
-    if number_inputted % 5 == 0:
+    if n % 5 == 0:
         output += "Plang"
 
-    if number_inputted % 7 == 0:
+    if n % 7 == 0:
         output += "Plong"
 
     if output == "":
-        output = str(number_inputted)
+        output = str(n)
 
     return output
 
