d7489a44f84d80fb04a6b9db74fa321be92e4122
Zaid Khider <ZKhider@spartaglobal.com>
2019-08-07 13:34:21 +0100

    The test has been edited

--- Test_for_code.py
+++ Test_for_code.py
@@ -1,25 +1,31 @@
 from app import raindrop_function #This line, imports the function raindrop_function app.py so it can be tested here.
-print("Here we will test the code to see if it works!")
-print("If the value printed is True then we know that the test has been successful.")
-
-print("Making sure that the numbers will return the correct word.")
-#Lines 6-9 check to see if the values would return the correct output.
-print(raindrop_function(3) == "Pling")
-print(raindrop_function(5) == "Plang")
-print(raindrop_function(7) == "Plong")
-print(raindrop_function(13) == "13")
 
 
-print("Testing numbers to see if they output combined raindrop sounds.")
-#Lines 13-17 check to see if the values would return the correct output.
-print(raindrop_function(2) == "2")
-print(raindrop_function(9) == "Pling")
-print(raindrop_function(21)) == "PlingPlong"
+def test_fator_3():
+    assert raindrop_function(3) == "Pling"
+    assert raindrop_function(9) == "Pling"
+
+def test_factor_5():
+    assert raindrop_function(5) == "Plang"
+    assert raindrop_function(10) == "Plang"
+
+def test_factor_7():
+    assert raindrop_function(7) == "Plong"
+    assert raindrop_function(14) == "Plong"
+
+def test_factor_3_and_5():
+    assert raindrop_function(30) == "PlingPlang"
+
+def test_factor_3_and_5_and_7():
+        assert raindrop_function(105) == "PlingPlangPlong"
+
+def test_factor_3_and_7():
+    assert raindrop_function(21) == "PlingPlong"
+
+def test_factor_5_and_7():
+    assert raindrop_function(35) == "PlangPlong"
 
 
-print("Testing inputs that should work including integers and whole numbers.")
-#Lines 30-31 check to see if the values would return the correct output.
-print(raindrop_function(27) == "PlingPlong")
-print(raindrop_function(27.0) == "PlingPlong")
-
+def test_factor_no_factor():
+    assert raindrop_function(13) == "13"
 
